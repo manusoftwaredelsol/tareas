d25e2fb6eac9bdf80eb79cb8c28593eef30f1be4
Fran García <fran7gm@gmail.com>
2021-05-06 13:03:03 +0200

    Update tareas.php
    Configuración base de datos

--- src/pages/tareas/tareas.php
+++ src/pages/tareas/tareas.php
@@ -1,8 +1,71 @@
 <?php
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$servername = "192.168.12.10";
+$database = "tareas";
+$username = "tareas";
+$password = "tareas";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Fallo conexión: " . mysqli_connect_error());
+}
+echo "Conectado correctamente";
+mysqli_close($conn);
+?>
 
-echo 'tareas';
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table, td, th {
+  border: 1px solid black;
+}
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+  text-align: center;
+}
+</style>
+</head>
+<body>
+
+<h2>Tareas</h2>
+<div>
+    <label>Ver</label><br/>
+	<select>
+		<option selected="selected">- Estados -</option>
+		<option>No iniciada</option>
+                <option>En curso</option>
+		<option>Completada</option>
+	</select>
+</div>
+<br/>
+<table>
+  <tr>
+    <th>Estado</th>
+    <th>Inicio</th>
+    <th>Fin</th>
+    <th>Descripción</th>
+  </tr>
+  <tr>
+    <td>aaa</td>
+    <td>aaa</td>
+    <td>aaa</td>
+    <td>aaa</td>
+  </tr>
+  <tr>
+    <td>bbb</td>
+    <td>bbb</td>
+    <td>bbb</td>
+    <td>bbb</td>
+  </tr>
+  <tr>
+    <td>ccc</td>
+    <td>ccc</td>
+    <td>ccc</td>
+    <td>ccc</td>
+  </tr>
+</table>
+</body>
+</html>
