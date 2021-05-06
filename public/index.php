a804093a0b73a8a7691985ec0b5863e88829b5e1
Manuel Moreno <manuelmorenodev@gmail.com>
2021-05-06 08:19:08 +0200

    Organización index 	- PHP antes de HTML 	- Cargar sesión siempre y al principio

--- public/index.php
+++ public/index.php
@@ -1,3 +1,34 @@
+<?php
+require '../src/config.php';
+
+$url  = parse_url($_SERVER['REQUEST_URI']);
+$path = trim($url['path'], ' /');
+$path = explode('/', $path);
+
+$pagina = $path[0] ?? '';
+
+ob_start();
+switch ($pagina) {
+    case 'userpage':
+        require '../src/pages/userpage/userpage.php';
+        break;
+
+    case 'register':
+        require '../src/pages/register/register.php';
+        break;
+
+    case 'login':
+        require '../src/pages/login/login.php';
+        break;
+
+    default:
+        http_response_code(404);
+        echo 'error 404';
+        exit;
+}
+
+$body = ob_get_clean();
+?>
 <html>
     <head>
         <meta charset="UTF-8">
@@ -5,37 +36,6 @@
         <link rel="stylesheet" href="../../../public/test.css">
     </head>
     <body>
-    
-        <?php
-        
-        require '../src/config.php';
-
-        $url  = parse_url($_SERVER['REQUEST_URI']);
-        $path = trim($url['path'], ' /');
-        $path = explode('/', $path);
-
-        $pagina = $path[0] ?? '';
-
-        switch ($pagina) {
-            case 'userpage':
-                require '../src/pages/userpage/userpage.php';
-            break;
-        
-            case 'register':
-                require '../src/pages/register/register.php';
-            break;
-        
-            case 'login':
-                require '../src/pages/login/login.php';
-                break;
-            
-            default:
-                http_response_code(404);
-                echo 'error 404';
-            exit;
-        }
-        ?>
-        
-        
+        <?php echo $body ?>
     </body>
 </html>
--- src/config.php
+++ src/config.php
@@ -3,10 +3,9 @@ define('USER', 'root');
 define('PASSWORD', '');
 define('HOST', 'localhost');
 define('DATABASE', 'test');
- 
+
 try {
     $connection = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
 } catch (PDOException $e) {
     exit("Error: " . $e->getMessage());
 }
-?>
